3d75fe3421a06e469937145ea20124dbaa2e5c40
itayyamin <itayyamin1@gmail.com>
2023-09-27 22:38:25 +0300

    add utils

---
+++ data_preprocessing/phonemes/const.py
@@ -0,0 +1,12 @@
+# PATH TO FOLDERS
+TIMIT_TRAIN_FOLDER_PATH = 'C:\\Users\\itayy\\Desktop\\engineering_projects\\timit\\train'
+TIMIT_TEST_FOLDER_PATH = 'C:\\Users\\itayy\\Desktop\\engineering_projects\\timit\\test'
+PROCESSED_TRAIN_FOLDER_PATH = 'C:\\Users\\itayy\\Desktop\\engineering_projects\\data_preparation\\phoneme_wav_files_train'
+PROCESSED_TEST_FOLDER_PATH = 'C:\\Users\\itayy\\Desktop\\engineering_projects\\data_preparation\\phoneme_wav_files_test'
+
+# PHONEMES CONSTS
+PHONEME_WORKING_SET = {"sh", "s", "w", "r", "d", "g", "l"}
+
+# PHONEMES PREPROCESS CONST PARAMETERS
+PADDING_IN_SAMPLES = 250
+PHONEME_REPRESENTATION_ARRAY_LENGTH = 2500
