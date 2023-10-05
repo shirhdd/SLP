c23b0b6ea09417d605494e35bae2a84f5f125f16
itayyamin <itayyamin1@gmail.com>
2023-10-05 18:56:28 +0300

    add function

--- data_preprocessing/data_visualization/spectrogram_utils.py
+++ data_preprocessing/data_visualization/spectrogram_utils.py
@@ -2,7 +2,7 @@ import librosa
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import spectrogram
-# from python_speech_features import mfcc
+from python_speech_features import mfcc
 
 
 def spectrogram_creation(y, sr, index=None, times=None):
@@ -32,7 +32,7 @@ def spectrogram_creation(y, sr, index=None, times=None):
         time_indices = [(int(start_time * sr), int(end_time * sr), name) for start_time, end_time, name in times]
 
     # Plot the spectrogram
-    plt.pcolormesh(time_samples, frequencies, 10 * np.log10(Sxx), shading='gouraud')
+    plt.pcolormesh(time_samples, frequencies, 10 * np.log10(Sxx), shading='gouraud',vmin=-170,vmax=-50)
     plt.colorbar(label='Intensity [dB]')
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
@@ -44,44 +44,53 @@ def spectrogram_creation(y, sr, index=None, times=None):
             plt.axvline(x=start_idx / sr, color='r', linestyle='--', label=name)
             plt.axvline(x=end_idx / sr, color='r', linestyle='--')
 
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper left')
     plt.show()
 
     return time_samples, frequencies, 10 * np.log10(Sxx)
 
 
-# def mfcc_spectrogram_creation(y, sr, index=None):
-#     """
-#     Compute and plot the MFCC spectrogram of the given audio signal.
-#
-#     Parameters:
-#     - y: array-like
-#         Audio signal.
-#     - sr: int
-#         Sampling rate of the audio signal.
-#     - index: tuple of int, optional
-#         Starting and ending indices to slice the audio signal. If provided, only this segment will be used.
-#     """
-#     if index:
-#         y = y[index[0]:index[1]]
-#     mfcc_features = mfcc(y, samplerate=sr, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512, lowfreq=0,
-#                          highfreq=None, preemph=0.97, winfunc=np.hamming)
-#
-#     # # Plot the MFCCs
-#     # # Transpose for the visualization to make time on x-axis
-#     # mfcc_features = mfcc_features.T
-#     # fig, ax = plt.subplots(figsize=(10, 4))
-#     # cax = ax.imshow(mfcc_features, aspect='auto', cmap='inferno', origin='lower')
-#     # fig.colorbar(cax, label='Intensity')
-#     # ax.set_ylabel('MFCC Coefficients')
-#     # ax.set_xlabel('Time Frame')
-#     # ax.set_title('MFCC Spectrogram sh1')
-#     # plt.tight_layout()
-#     # plt.show()
-#     return mfcc_features
+def mfcc_spectrogram_creation(y, sr, index=None):
+    """
+    Compute and plot the MFCC spectrogram of the given audio signal.
+
+    Parameters:
+    - y: array-like
+        Audio signal.
+    - sr: int
+        Sampling rate of the audio signal.
+    - index: tuple of int, optional
+        Starting and ending indices to slice the audio signal. If provided, only this segment will be used.
+    """
+    if index:
+        y = y[index[0]:index[1]]
+    mfcc_features = mfcc(y, samplerate=sr, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512, lowfreq=0,
+                         highfreq=None, preemph=0.97, winfunc=np.hamming)
+
+    # Plot the MFCCs
+    # Transpose for the visualization to make time on x-axis
+    mfcc_features = mfcc_features.T
+    fig, ax = plt.subplots(figsize=(10, 4))
+    cax = ax.imshow(mfcc_features, aspect='auto', cmap='inferno', origin='lower')
+    fig.colorbar(cax, label='Intensity')
+    ax.set_ylabel('MFCC Coefficients')
+    ax.set_xlabel('Time Frame')
+    ax.set_title('MFCC Spectrogram sh1')
+    plt.tight_layout()
+    plt.show()
+    return mfcc_features
 
 
-# y, sr = librosa.load('C:\\Users\\itayy\\Desktop\\engineering_projects\\shalom_example.wav', sr=16000)
-# spectrogram_creation(y, sr, index=None)
-# s = mfcc_spectrogram_creation(y, sr,
-#                               index=None)
+y, sr = librosa.load('C:\\Users\\itayy\\Desktop\\wrods\\thing.wav', sr=16000)
+# spectrogram_creation(y, sr, index=None,times=[(1.08, 1.2, 'S'), (1.2, 1.3, 'IH'), (1.3, 1.53, 'NG'), (1.53, 1.93, '[SIL]')])
+# spectrogram_creation(y, sr, index=None,times=[(0.0, 1.08, '[SIL]'), (1.08, 1.22, 'TH'), (1.22, 1.3, 'IH'), (1.3, 1.53, 'NG'), (1.53, 1.93, '[SIL]')])
+spectrogram_creation(y, sr, index=(int(1.3 * 16000),int(1.53 * 16000)))
+# s = mfcc_spectrogram_creation(y, sr,index=None)
+
+# thing -> sing ( thing with sing transcript )
+# ([(0.0, 1.08, '[SIL]'), (1.08, 1.2, 'S'), (1.2, 1.3, 'IH'), (1.3, 1.53, 'NG'), (1.53, 1.93, '[SIL]')],
+# [(0.0, 1.08, '[SIL]'), (1.08, 1.53, 'sing'), (1.53, 1.93, '[SIL]')])
+
+# thing -> thing ( thing with thing transcript )
+# ([(0.0, 1.08, '[SIL]'), (1.08, 1.22, 'TH'), (1.22, 1.3, 'IH'), (1.3, 1.53, 'NG'), (1.53, 1.93, '[SIL]')],
+# [(0.0, 1.08, '[SIL]'), (1.08, 1.53, 'thing'), (1.53, 1.93, '[SIL]')])
--- speech_recognition/wav_to_text.py
+++ speech_recognition/wav_to_text.py
@@ -2,10 +2,9 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-''' recording the sound '''
-
 
 def textIt(file_path):
+
     with sr.AudioFile(file_path) as source:
         recorded_audio = recognizer.listen(source)
         print("Done recording")
@@ -18,11 +17,7 @@ def textIt(file_path):
             language="en-US"
         )
         print("Decoded Text : {}".format(text))
+        return text
 
     except Exception as ex:
-        print(ex)
-
-
-textIt("C:\\Users\\itayy\\Desktop\\SLP\\thought.wav")
-
-
+        print(f"error while decode: ", ex)
